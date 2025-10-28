5077d595f4af85e31c735599b89f0278d34853a9
Geek_Joystick <contact.geekjoystick@gmail.com>
2025-10-28 18:08:16 +0100

    Fixes and forgetness

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private PhysicsController _physicsController;
+
+    private void Awake()
+    {
+        _physicsController = GetComponent<PhysicsController>();
+    }
+
+    private void FixedUpdate()
+    {
+        _physicsController.SetMovementDirection(Vector3.forward * Mathf.Sign(Mathf.Sin(Time.time)));
+    }
+}
