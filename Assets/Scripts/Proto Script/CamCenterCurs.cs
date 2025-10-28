a5d4f79e2b4fbe5085e8c2fade1baad53ae40b34
PierreAC3 <pierreantoine.chalaron@gmail.com>
2025-10-28 22:21:16 +0100

    Camera Doly Rework

---
+++ Assets/Scripts/Proto Script/CamCenterCurs.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CamCenterCurs : MonoBehaviour
+{
+    [Header("CentreCursor")]
+    [SerializeField] private GameObject CursorA;
+    [SerializeField] private GameObject CursorB;
+    private Vector3 distance;
+
+    [Header("Zoom")] 
+    [SerializeField] private AnimationCurve _curve;
+
+    [SerializeField] private CinemachineCamera _cinemachineCamera;
+    
+
+
+    private void FixedUpdate()
+    {
+        distance = (CursorA.transform.position - CursorB.transform.position)/2;
+        transform.position = CursorA.transform.position - distance+new Vector3(-9.1f,11.6f,-14f);
+
+
+        var zoom = _curve.Evaluate(distance.sqrMagnitude);
+        _cinemachineCamera.Lens.OrthographicSize = zoom;
+    }
+}
--- Assets/Scripts/Proto Script/Lever.cs
+++ Assets/Scripts/Proto Script/Lever.cs
@@ -1,17 +1,20 @@
 using System;
 using UnityEngine;
+using Quaternion = System.Numerics.Quaternion;
 
 public class Lever : Interactable
 {
+    [SerializeField] private int _channel;
+    
     private bool OnOff;
 
     private GameObject _pivot;
     [SerializeField] private AnimationCurve _animationCurve;
+    private float time;
 
     private void Awake()
     {
         _pivot = this.gameObject.transform.GetChild(0).gameObject;
-        Debug.Log(_pivot);
     }
 
     protected override void OnClick()
@@ -19,15 +22,34 @@ public class Lever : Interactable
         if (OnOff)
         {
             OnOff = false;
+            SwitchManager.SetSwitch(_channel,true);
         }
         else
         {
             OnOff = true;
+            SwitchManager.SetSwitch(_channel,false);
         }
     }
 
     private void FixedUpdate()
     {
-        //_pivot.transform.rotation = 
+        var value = _animationCurve.Evaluate(time);
+        _pivot.transform.rotation = UnityEngine.Quaternion.Euler(new Vector3(0,0,value));
+        if (OnOff)
+        {
+            time +=Time.deltaTime;
+            if (time >= _animationCurve.length)
+            {
+                time = _animationCurve.length;
+            }
+        }
+        else
+        {
+            time -=Time.deltaTime;
+            if (time <= 0)
+            {
+                time = 0;
+            }
+        }
     }
 }
